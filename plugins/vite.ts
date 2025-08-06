fec23ff76e9829839fc6d2115a3c17f76bf433cb
Abu Taher Muhammad <abut1081@gmail.com>
2025-08-06 10:39:22 +0600

    feat: add Vite plugin for class minification

---
+++ plugins/vite.ts
@@ -0,0 +1,18 @@
+import { runClassMinify, RunnerOptions } from "../src/runner";
+
+export interface ClassMinifyVitePluginOptions extends RunnerOptions {}
+
+export default function classMinifyPlugin(
+  options: ClassMinifyVitePluginOptions
+) {
+  return {
+    name: "vite:class-minify",
+    apply: "build",
+    enforce: "post",
+
+    async generateBundle() {
+      // Transformation logic goes here
+      await runClassMinify(options);
+    },
+  };
+}
